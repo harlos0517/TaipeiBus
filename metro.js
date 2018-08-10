bac78c30aa7051da0dd476037433e6a94a5da10c
DeemoHarlos <cces4415@gmail.com>
2018-08-10 20:34:47 +0800

    BIG UPDATE LOL

---
+++ metro.js
@@ -0,0 +1,37 @@
+var metro = [
+	{"id": 1, "length": 10102.693, "color":"#E3002C", "lineName": "淡水線"    ,"routes": [{
+		"routeName": "往淡水", "sections": 
+			[  1,  3,  5,  7,  9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 43, 45, 47, 49, 51, 53]},{
+		"routeName": "往象山", "sections": 
+			[ 54, 52, 50, 48, 46, 44, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10,  8,  6,  4,  2]}]},
+	{"id": 2, "length":  1629.435, "color": "#0070BD", "lineName": "板南線"    ,"routes": [{
+		"routeName": "往南港展覽館", "sections": 
+			[ 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97]},{
+		"routeName": "往頂埔", "sections": 
+			[ 98, 96, 94, 92, 90, 88, 86, 84, 82, 80, 78, 76, 74, 72, 70, 68, 66, 64, 62, 60, 58, 56]}]},
+	{"id": 3, "length":     9.394, "color": "#008659", "lineName": "松山新店線","routes": [{
+		"routeName": "往", "sections": 
+			[ 99,101,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133]},{
+		"routeName": "往", "sections": 
+			[134,132,130,128,126,124,122,120,118,116,114,112,110,108,106,102,100]}]},
+/*	{"id": 4, "length":  4278.94 , "color": "#C48C31", "lineName": "文湖線"   ,"routes": [{
+		"routeName": "往", "sections": 
+			[]},{
+		"routeName": "往", "sections": 
+			[]}]},
+	{"id": 5, "length":  6402.58 , "color": "#F8B61C", "lineName": "中和新蘆線","routes": [{
+		"routeName": "往", "sections": 
+			[]},{
+		"routeName": "往", "sections": 
+			[]}]},*/
+	{"id": 6, "length":     0    , "color": "#FD92A3", "lineName": "新北投支線","routes": [{
+		"routeName": "往新北投", "sections": 
+			[ 41]},{
+		"routeName": "往北投", "sections": 
+			[ 42]}]},
+	{"id": 7, "length":     0    , "color": "#CFDB00", "lineName": "小碧潭支線","routes": [{
+		"routeName": "往小碧潭", "sections": 
+			[103]},{
+		"routeName": "往七張", "sections": 
+			[104]}]}
+]
