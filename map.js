f8a9645927deaedeabe9fe11ad8d722e785c94c6
DeemoHarlos <cces4415@gmail.com>
2018-09-12 17:36:49 +0800

    update with server

--- map.js
+++ map.js
@@ -29,92 +29,63 @@ function getData(e,f){
 }
 
 getData('metroStop',function(metroStop){
-	getData('metroSection',function(metroSection){
-		getData('metroRoute',function(metroRoute){
-			var routes = map.append('g').attr('id','routes')
-			function findStop   (_id){ return metroStop.find(function (x){ return (x._id==_id) }) }
-			function findSection(_id){
-				var section = {"start": 0,"end": 0, "coordinates": []}
-				var found = metroSection.find(function (x){ return (x._id==_id) })
-				if (found.reverseOf) {
-					found = metroSection.find(function (x){ return (x._id==found.reverseOf) })
-					section.start = found.endStation
-					section.end   = found.startStation
-					section.coordinates = found.coordinates.slice()
-					section.coordinates.reverse()
-					section.coordinates.splice(0,0,findStop(section.start).coordinates)
-					section.coordinates.push(findStop(section.end).coordinates)
-				}
-				else {
-					section.start = found.startStation
-					section.end   = found.endStation
-					section.coordinates = found.coordinates.slice()
-					section.coordinates.splice(0,0,findStop(section.start).coordinates)
-					section.coordinates.push(findStop(section.end).coordinates) 
-				}
-				return section
-			}
-			function drawStop   (g,id,strokeColor,radius){
-				g.append('circle').attr('class','stop')
-					.attr('cx',lontrans(findStop(id).coordinates[0]))
-					.attr('cy',lattrans(findStop(id).coordinates[1]))
-					.attr('r',radius).style('stroke',strokeColor)
-			}
-			function drawSection(g,id,strokeColor){
-				var thisSection = findSection(id)
-				g.append('path').data([thisSection.coordinates]).attr('class','section')
-					.style('stroke',strokeColor).attr('d',d3.line()
-					.x(function(d){return lontrans(d[0])})
-					.y(function(d){return lattrans(d[1])}))
-				return thisSection
-			}
+	getData('metroRoute',function(metroRoute){
+		var routes = map.append('g').attr('id','routes')
+		function findStop   (_id){ return metroStop.find(function (x){ return (x._id==_id) }) }
+		function drawStop   (g,id,strokeColor,radius){
+			g.append('circle').attr('class','stop')
+				.attr('cx',lontrans(findStop(id).coordinates[0]))
+				.attr('cy',lattrans(findStop(id).coordinates[1]))
+				.attr('r',radius).style('stroke',strokeColor)
+		}
 
-			metroRoute.forEach(function(le,li,la){
-				var thisLine = routes.append('g')
-					.attr('id',le.lineName)
-					.attr('_id',le._id)
+		metroRoute.forEach(function(le,li,la){
+			var thisLine = routes.append('g')
+				.attr('id',le.lineName)
+				.attr('_id',le._id)
 
-				le.routes.forEach(function(re,ri,ra){
-					var thisRoute    = thisLine .append('g').attr('id',re.routeName+'-route'   )
-					var thisSections = thisRoute.append('g').attr('id',re.routeName+'-sections')
-					var thisStops    = thisRoute.append('g').attr('id',re.routeName+'-stops'   )
+			le.routes.forEach(function(re,ri,ra){
+				var thisRoute = thisLine .append('g').attr('id',re.routeName+'-route')
+				var thisPaths = thisRoute.append('g').attr('id',re.routeName+'-paths')
+				var thisStops = thisRoute.append('g').attr('id',re.routeName+'-stops')
 
-					var thisStop = findSection(re.sections[0]).start
-					drawStop(thisStops,thisStop,le.color,5)
-					re.sections.forEach(function(se,si,sa){
-						var thisSection = drawSection(thisSections,se,le.color)
-						drawStop(thisStops,thisSection.end,le.color,5)
-					})
+				thisPaths.append('path').data([re.stops]).attr('class','path')
+					.style('stroke',le.color).attr('d',d3.line()
+					.x(function(d){return lontrans(findStop(d.stopId).coordinates[0])})
+					.y(function(d){return lattrans(findStop(d.stopId).coordinates[1])}))
+
+				re.stops.forEach(function(se,si,sa){
+					drawStop(thisStops,se.stopId,le.color,5)
 				})
 			})
-
-			var stops = map.append('g').attr('id','stops')
-			metroStop.forEach(function(e,i,a){
-				var thisStop = stops.append('g')
-					.attr('id','stop-'+e.id)
-					.attr('_id',e._id)
-				thisStop.append('circle')
-					.attr('cx',lontrans(e.coordinates[0]))
-					.attr('cy',lattrans(e.coordinates[1]))
-					.attr('r','3')
-				thisStop.append('text').text(e.name.zh)
-					.attr('text-anchor','middle')
-					.attr('x',lontrans(e.coordinates[0]) + 0)
-					.attr('y',lattrans(e.coordinates[1]) + 16)
-			})
-
-			var metroStopTable = d3.select('body').select('#metroStop-table').select('table')
-			metroStop.reverse().forEach(function(e,i,a){
-				var tr = metroStopTable.append('tr')
-				tr.append('td').text(e.id)
-				tr.append('td').text(e.code)
-				tr.append('td').text(e.name.zh)
-				tr.append('td').text(e.name.en)
-				tr.append('td').text(e.coordinates[0])
-				tr.append('td').text(e.coordinates[1])
-			})
-
 		})
+
+		var stops = map.append('g').attr('id','stops')
+		metroStop.forEach(function(e,i,a){
+			var thisStop = stops.append('g')
+				.attr('id','stop-'+e.id)
+				.attr('_id',e._id)
+			thisStop.append('circle')
+				.attr('cx',lontrans(e.coordinates[0]))
+				.attr('cy',lattrans(e.coordinates[1]))
+				.attr('r','3')
+			thisStop.append('text').text(e.name.zh)
+				.attr('text-anchor','middle')
+				.attr('x',lontrans(e.coordinates[0]) + 0)
+				.attr('y',lattrans(e.coordinates[1]) + 16)
+		})
+/*
+		var metroStopTable = d3.select('body').select('#metroStop-table').select('table')
+		metroStop.reverse().forEach(function(e,i,a){
+			var tr = metroStopTable.append('tr')
+			tr.append('td').text(e.id)
+			tr.append('td').text(e.code)
+			tr.append('td').text(e.name.zh)
+			tr.append('td').text(e.name.en)
+			tr.append('td').text(e.coordinates[0])
+			tr.append('td').text(e.coordinates[1])
+		})
+*/
 	})
 })
 
