9e82e306f1951df5071ccba41407df7f0cb49cd4
DeemoHarlos <cces4415@gmail.com>
2022-05-11 11:29:16 +0800

    cors

--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 tmux new-session -d 'cd frontend; yarn dev'
-tmux split-window -h 'cd backend; http-server -p 1337'
+tmux split-window -h 'cd backend; http-server -p 1337 --cors'
 tmux -2 attach-session -d
